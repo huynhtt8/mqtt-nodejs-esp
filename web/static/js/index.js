9f0edeaa1f6f6a829731a47c177652b5f8d3cb27
huynh.trinhthe <huynh.trinhthe@git.evotek.vn>
2020-06-10 00:05:04 +0700

    test mcb on class

--- web/static/js/index.js
+++ web/static/js/index.js
@@ -2,11 +2,11 @@ const ctx = document.getElementById('myChart').getContext('2d');
 const chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: [],
+        labels: [1,2,3,4,5,6,7,8],
         datasets: [{
             label: "Temperature",
             borderColor: "#28a745",
-            data: [],
+            data: [20, 21, 24, 20, 25, 21, 24, 27],
             fill: false,
             pointStyle: 'circle',
             backgroundColor: '#1cc88a',
@@ -17,7 +17,7 @@ const chart = new Chart(ctx, {
         {
             label: "Humidity",
             borderColor: "#17a2b8",
-            data: [],
+            data: [70, 71, 73, 75, 70, 71, 73, 75],
             fill: false,
             pointStyle: 'circle',
             backgroundColor: '#5468ca',
@@ -30,46 +30,48 @@ const chart = new Chart(ctx, {
 });
 
 const socket = io('http://localhost:3000');
-socket.on('htn/sensors', (data) => {
-    console.log(data);
-    const date = new Date(data.datetime);
-    const time = date.getHours() + "H-" + date.getMinutes() + "M-" + date.getSeconds() + "S";
-    const temperatureBox = document.getElementById("temperature-box");
-    const temperatureBoxContainer = document.getElementById("temperature-box-container");
-    temperatureBox.innerText = data.temp;
-
-    const humidityBox = document.getElementById("humidity-box");
-    const humidityBoxContainer = document.getElementById("humidity-box-container");
-    humidityBox.innerText = data.humi;
-
-    if(data.temp < 10 || data.temp > 40) {
-        temperatureBoxContainer.style.backgroundColor = "#fc0a02";
-    } else if(data.temp < 20 || data.temp > 35) {
-        temperatureBoxContainer.style.backgroundColor = "#ffc107";
-    } else {
-        temperatureBoxContainer.style.backgroundColor = "#28a745";
-    }
-
-    if(data.humi < 40 || data.humi > 80) {
-        humidityBoxContainer.style.backgroundColor = "#fc0a02";
-    } else if(data.humi < 50 || data.humi > 70) {
-        humidityBoxContainer.style.backgroundColor = "#ffc107";
-    } else {
-        humidityBoxContainer.style.backgroundColor = "#17a2b8";
-    }
-
-    if (chart.data.labels.length !== 15) { //If we have less than 15 data points in the graph
-        chart.data.labels.push(time);  //Add time in x-asix
-        chart.data.datasets[0].data.push(data.temp);
-        chart.data.datasets[1].data.push(data.humi);
-    } else { //If there are already 15 data points in the graph.
-        chart.data.labels.shift(); //Remove first time data
-        chart.data.labels.push(time); //Insert latest time data
-        chart.data.datasets[0].data.push(data.temp);
-        chart.data.datasets[1].data.push(data.humi);
-    }
-    chart.update(); //Update the graph.
-});
+// socket.on('htn/sensors', (data) => {
+//     console.log(data);
+//     const date = new Date(data.datetime);
+//     const time = date.getHours() + "H-" + date.getMinutes() + "M-" + date.getSeconds() + "S";
+//     const temperatureBox = document.getElementById("temperature-box");
+//     const temperatureBoxContainer = document.getElementById("temperature-box-container");
+//     temperatureBox.innerText = data.temp;
+//
+//     const humidityBox = document.getElementById("humidity-box");
+//     const humidityBoxContainer = document.getElementById("humidity-box-container");
+//     humidityBox.innerText = data.humi;
+//
+//     if(data.temp < 10 || data.temp > 40) {
+//         temperatureBoxContainer.style.backgroundColor = "#fc0a02";
+//     } else if(data.temp < 20 || data.temp > 35) {
+//         temperatureBoxContainer.style.backgroundColor = "#ffc107";
+//     } else {
+//         temperatureBoxContainer.style.backgroundColor = "#28a745";
+//     }
+//
+//     if(data.humi < 40 || data.humi > 80) {
+//         humidityBoxContainer.style.backgroundColor = "#fc0a02";
+//     } else if(data.humi < 50 || data.humi > 70) {
+//         humidityBoxContainer.style.backgroundColor = "#ffc107";
+//     } else {
+//         humidityBoxContainer.style.backgroundColor = "#17a2b8";
+//     }
+//
+//     if (chart.data.labels.length !== 10) {
+//         chart.data.labels.push(time);
+//         chart.data.datasets[0].data.push(data.temp);
+//         chart.data.datasets[1].data.push(data.humi);
+//     } else {
+//         chart.data.labels.shift();
+//         chart.data.labels.push(time);
+//         chart.data.datasets[0].data.shift();
+//         chart.data.datasets[0].data.push(data.temp);
+//         chart.data.datasets[1].data.shift()
+//         chart.data.datasets[1].data.push(data.humi);
+//     }
+//     chart.update(); //Update the graph.
+// });
 
 
 function chargeStatusLedOne() {
